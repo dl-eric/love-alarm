703ab6d748a3621102ffa2e55b7b98a6b0070d1d
Eric Lee <ericdl2@illinois.edu>
2019-11-25 01:10:50 -0500

    Handle errors properly

--- app/api/endpoints/profiles.py
+++ app/api/endpoints/profiles.py
@@ -1,27 +1,28 @@
 from bson.objectid import ObjectId
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, HTTPException
 from starlette.websockets import WebSocket
 import asyncio
 
 from db.base import db, r
 from db.models import ForeignProfile
 
+
 router = APIRouter()
 
 @router.get('/me')
 async def me():
-    return "It's me"
+    return "It's me!"
 
 @router.patch('/me')
 async def update_me():
-    return "Patch me"
+    return "Path me!"
 
 @router.websocket('/me/ws')
 async def ws_endpoint(websocket: WebSocket):
     await websocket.accept()
-    a = 0
+
     while True:
-        a += 1
+        # User should send us their location and id
         position = await websocket.receive_text()
 
         # Message format: "longitude:latitude:userid"
@@ -37,10 +38,15 @@ async def ws_endpoint(websocket: WebSocket):
 
 @router.get('/{profile_id}', response_model=ForeignProfile)
 async def get_profile(profile_id: str):
-    profile = db.User.find_one({'_id': ObjectId(profile_id)}, {'_id': 0})
-    return profile
+    # TODO: Also send distance information
+    try:
+        obj_profile_id = ObjectId(profile_id)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid ID")
 
-@router.get('/')
-async def get_nearby(token: int = Header(None)):
-    print(token)
-    return 0
+    profile = db.User.find_one({'_id': obj_profile_id}, {'_id': 0})
+
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+
+    return profile
--- app/api/endpoints/wink.py
+++ app/api/endpoints/wink.py
@@ -1,5 +1,5 @@
 from bson.objectid import ObjectId
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from db.base import db
 from db.models import UserId
@@ -8,18 +8,51 @@ router = APIRouter()
 
 @router.post('/{profile_id}')
 async def wink_profile(profile_id: str, curr_user: UserId):
-    user_id = ObjectId(profile_id)
+    try:
+        user_id = ObjectId(profile_id)
+        ObjectId(curr_user.user_id)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid ID")
+    
+    if profile_id == curr_user.user_id:
+        raise HTTPException(status_code=400, detail="Can't wink at yourself")
 
-    winkers = db.User.find_one({'_id': user_id})['winkers']
+    profile = db.User.find_one({'_id': user_id})
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+
+    winkers = profile['winkers']
+    
+    if curr_user.user_id in winkers:
+        raise HTTPException(status_code=400, detail="Already winked this user")
+
     winkers.append(curr_user.user_id)
     db.User.update({'_id': user_id}, { '$set': {'winkers': winkers}})
 
-    return profile_id
+    return {"message": "Successfully winked"}
 
-@router.post('/{profile_id}')
+@router.delete('/{profile_id}')
 async def unwink_profile(profile_id: str, curr_user: UserId):
-    return profile_id
+    try:
+        user_id = ObjectId(profile_id)
+        ObjectId(curr_user.user_id)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid ID")
+    
+    if profile_id == curr_user.user_id:
+        raise HTTPException(status_code=400, detail="Can't unwink at yourself")
 
-@router.get('/')
-async def get_winks():
-    return 0
+    profile = db.User.find_one({'_id': user_id})
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+
+    winkers = profile['winkers']
+
+    try:
+        winkers.remove(curr_user.user_id)
+    except:
+        raise HTTPException(status_code=400, detail="Already unwinked/not winked")
+
+    db.User.update({'_id': user_id}, { '$set': {'winkers': winkers}})
+
+    return {"message": "Successfully unwinked"}
