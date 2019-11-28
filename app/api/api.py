73ddf7515b2cfea7d5efb5f7753a2b2ee2653fa4
Eric Lee <ericdl2@illinois.edu>
2019-11-28 08:48:02 -0500

    Add tags

--- app/api/api.py
+++ app/api/api.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from .endpoints import auth, profiles, wink
 
 api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth")
-api_router.include_router(profiles.router, prefix="/profiles")
-api_router.include_router(wink.router, prefix="/wink")
+api_router.include_router(auth.router, prefix="/auth", tags=['Auth'])
+api_router.include_router(profiles.router, prefix="/profiles", tags=['Profiles'])
+api_router.include_router(wink.router, prefix="/wink", tags=['Winking'])
