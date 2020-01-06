da8622d021e23392cf8f9696c50c60532e7dd520
Eric <ericdl712@gmail.com>
2020-01-05 21:49:34 -0500

    Add warning if started in debug mode

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from starlette.websockets import WebSocket
 
 from .api import api
-from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, DEBUG_MODE
 
+if DEBUG_MODE:
+    print("***WARNING 1: STARTING IN DEBUG MODE***")
+    print("***WARNING 2: STARTING IN DEBUG MODE***")
+    print("***WARNING 3: STARTING IN DEBUG MODE***")
 
 # FastAPI
 app = FastAPI()
